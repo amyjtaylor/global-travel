88f3c17c5c9cae4adbc3ba036e5ab53d43981ec6
amyjtaylor <a.jtaylor@hotmail.co.uk>
2024-08-06 18:24:10 +0100

    Add files via upload

---
+++ GP-contact.js
@@ -0,0 +1,5 @@
+function pressAlert(){
+    let alertMessage = "button has been clicked!";
+    console.log("alertMessage");
+}
+document.getElementById("myButton").addEventListener("click", pressAlert);
